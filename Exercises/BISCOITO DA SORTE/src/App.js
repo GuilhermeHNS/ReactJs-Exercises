2546ea641842f93e1ae1efd728987ea3b1445420
Guilherme Henrique Novaes de Souza <guilhermehenriquenovaes@gmail.com>
2021-03-12 08:24:00 -0300

    First commit

---
+++ Exercises/BISCOITO DA SORTE/src/App.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react'
+
+class App extends Component{
+
+    constructor(props){
+        super(props)
+        this.state = {
+            hora: '00:00:00'
+        }
+    }
+
+    render(){
+        return(
+            
+        )
+    }
+} 
+
+export default App
