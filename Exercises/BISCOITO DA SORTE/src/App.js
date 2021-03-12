4538a4ce594fc5de7a48eaecb3722d0b2108781e
Guilherme Henrique Novaes de Souza <guilhermehenriquenovaes@gmail.com>
2021-03-12 11:31:09 -0300

    Changing the image
    Changing the image by clicking the button

--- Exercises/BISCOITO DA SORTE/src/App.js
+++ Exercises/BISCOITO DA SORTE/src/App.js
@@ -1,17 +1,46 @@
 import React, {Component} from 'react'
-
+import Botao from './Components/index'
+import Biscoito from './assets/biscoito.png'
+import Teste from './assets/teste.jpg'
+import Style from './Styles/style.css'
 class App extends Component{
 
     constructor(props){
         super(props)
         this.state = {
-            hora: '00:00:00'
+            frasesExibidas: ''
         }
+        this.frases = [
+            'A vida trará coisas boas se tiveres paciência.',
+            'Demonstre amor e alegria em todas as oportunidades e verás que a paz nasce dentro de você.',
+            'Não compense na ira o que lhe falta na razão.',
+            'Defeitos e virtudes são apenas dois lados da mesma moeda.',
+            'A maior de todas as torres começa no solo.',
+            'Não há que ser forte. Há que ser flexível.'
+        ]
+
+        this.imagem = Biscoito
+        this.quebraBiscoito = this.quebraBiscoito.bind(this)
+    }
+
+    quebraBiscoito(){
+        let state = this.state
+        let numeroAleatorio = Math.floor(Math.random() * this.frases.length)
+        state.frasesExibidas = '"'+this.frases[numeroAleatorio]+'"'
+        this.setState(state)
+
+        this.imagem = Teste
+
     }
 
     render(){
         return(
-            
+            <div className="container">
+                <img src={this.imagem} alt="biscoito" className="imagemBiscoito"></img>
+
+                <Botao nome="Abrir biscoito" acaoBtn={this.quebraBiscoito}/>
+                <h3 className="fraseExibida">{this.state.frasesExibidas}</h3>
+            </div>
         )
     }
 } 
